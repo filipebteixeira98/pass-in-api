bda4ef2d4cdf76d1fed994efba6be0018cd132c5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-17 15:29:49 -0300

    chore: configure prisma and database client dependency

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('pass-in');
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'pass-in'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Server is running at http://localhost:3333/')
+})
