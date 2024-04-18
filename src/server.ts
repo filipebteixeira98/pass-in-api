3c1c7c87859d62b2ec6b8a96dc08f18493e445b9
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-17 22:31:20 -0300

    feat(app): add route that allows users to register attendee for an event

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { createEvent } from './routes/create-event'
+import { registerForEvent } from './routes/register-for-event'
 
 const app = fastify()
 
@@ -13,6 +14,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
+
+app.register(registerForEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 Server is running at http://localhost:3333/')
