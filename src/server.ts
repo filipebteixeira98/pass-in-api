185ddbed406a527b15607f1eb5d991f67511bfb1
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-18 04:48:05 -0300

    feat(app): add route that allows users to get information about given event

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
 
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
+import { getEvent } from './routes/get-event'
 
 const app = fastify()
 
@@ -16,6 +17,8 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
 
 app.register(registerForEvent)
+
+app.register(getEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 Server is running at http://localhost:3333/')
